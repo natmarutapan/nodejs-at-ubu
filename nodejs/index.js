fc6762bd476383e588a679061486649b0efd624d
natmarutapan <natthapol.ma.57@ubu.ac.th>
2017-10-03 19:26:57 +0700

    update 3/10/2560

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,5 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World! from 5711403229 Natthapol Marutapan' );
+    res.end('Hello World! from 5711403229 Natthapol Marutapan  update at 3/10/2017 ' );
+    //res.end('update at 3/10/2017' );
 }).listen(8080);
